d85943bc1a37f6846d62d59c9018d03c55811534
racsandoval <sandoval.ac.rafael@gmail.com>
2021-01-13 14:32:13 -0300

    add required fields to graphql types

--- src/graphql-schema/schema.ts
+++ src/graphql-schema/schema.ts
@@ -3,15 +3,15 @@ import { gql } from 'apollo-server-express';
 export const schema = gql`
   type User {
     id: ID!
-    name: String
-    email: String
-    birthDate: String
-    cpf: String
+    name: String!
+    email: String!
+    birthDate: String!
+    cpf: String!
   }
 
   type Login {
-    user: User
-    token: String
+    user: User!
+    token: String!
   }
 
   type Query {
