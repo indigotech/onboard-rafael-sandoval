cbece7b911951be301991616c12d980695b83010
racsandoval <sandoval.ac.rafael@gmail.com>
2021-01-18 15:28:15 -0300

    add user seed generator

---
+++ src/data/db/seed/user.ts
@@ -0,0 +1,30 @@
+import { dbConnection } from '@data/db/connection';
+import { User } from '@data/db/entity/user';
+import { setEnv } from 'setup';
+import * as faker from 'faker';
+import { getRepository } from 'typeorm';
+import { hash } from '@core/authentication';
+
+const userSeed = async () => {
+  setEnv();
+  await dbConnection();
+
+  const repo = getRepository(User);
+
+  const userList: User[] = [];
+
+  for (let i = 0; i < 50; i++) {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      birthDate: faker.date.past(),
+      cpf: '12345678900',
+      password: hash('senha123'),
+    };
+    const userEntity = repo.create(user);
+    userList.push(userEntity);
+  }
+  repo.save(userList);
+};
+
+userSeed();
