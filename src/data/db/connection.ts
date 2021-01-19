f4e30d75ff84873557ddbf636d9b205468919a49
racsandoval <sandoval.ac.rafael@gmail.com>
2021-01-19 12:18:43 -0300

    add address entity

--- src/data/db/connection.ts
+++ src/data/db/connection.ts
@@ -1,11 +1,12 @@
 import { User } from '@data/db/entity/user';
+import { Address } from '@data/db/entity/address';
 import { createConnection, Connection } from 'typeorm';
 
 export const dbConnection = (): Promise<Connection> => {
   return createConnection({
     type: 'postgres',
     url: process.env.DB_URL,
-    entities: [User],
+    entities: [User, Address],
     synchronize: true,
   });
 };
---
+++ src/data/db/entity/address.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '@data/db/entity/user';
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  street: string;
+
+  @Column()
+  streetNumber: string;
+
+  @Column({ nullable: true })
+  complement: string;
+
+  @Column()
+  neighborhood: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @ManyToOne(() => User, (user) => user.addresses)
+  user: User;
+}
